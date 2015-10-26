bb88b966ea3f1739c5e1a42f6145e4eea45e9bb5
TheGamingSprite <Jake.stanley2004@gmail.com>
2015-10-26 07:17:27 +0000

    Update and rename Command_lolno.java to Command_Hailno.java
    HailNo

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_Hailno.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_Hailno.java
@@ -5,14 +5,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Lolno.", usage = "/<command>")
+@CommandPermissions(level = AdminLevel.TELNET, source = SourceType.BOTH)
+@CommandParameters(description = "HailNo.", usage = "/<command>")
 public class Command_lolno extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        sender_p.chat("Lolno. Just.. just.. go. just GO AWAY!");
+        sender_p.chat("HailNo.go Away or get Ban Hammered");
         Bukkit.dispatchCommand(sender, "expel");
         return true;
     }
